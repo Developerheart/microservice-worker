b13ea3b83f2f9bb51c6459f472ebd30b16ec3272
Developerheart <leonardonascimento897@gmail.com>
2022-03-09 22:07:33 -0300

    initial commit

---
+++ src/test/java/io/github/developerheart/microserviceworker/MicroserviceWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.developerheart.microserviceworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
